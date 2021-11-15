2f8548b5c85eaa895041fd090f60f33856f36d64
Sherry Yeh <b08705053@ntu.edu.tw>
2021-11-15 16:33:05 +0800

    remove database.py gitignore

---
+++ Dorm_Service_Backend/App/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine("postgresql://postgres:wujenny1218@localhost/dorm_service", echo = True)
+
+
+# 葉的
+# import os
+# from dotenv import load_dotenv
+# load_dotenv()
+# POSGRES_URI = os.getenv('POSGRES_URI')
+# engine = create_engine(POSGRES_URI, echo = True)
+
+
+Base = declarative_base()
+
+SessionLocal = sessionmaker(bind=engine)
+
+
+
+#done setting up database
