eccb1b5ba475c72309c22120d3e91463053e9c98
WU,CHIEN YIN <wcy881218@gmail.com>
2021-11-17 01:30:24 +0800

    change some color

--- Dorm_Service_Frontend/frontend/craco.config.js
+++ Dorm_Service_Frontend/frontend/craco.config.js
@@ -7,7 +7,14 @@ module.exports = {
       options: {
         lessLoaderOptions: {
           lessOptions: {
-            modifyVars: { "@primary-color": "#1DA57A" },
+            modifyVars: { 
+              "@primary-color": "#138796",
+              "@error-color": "#F17FB2",
+              "@body-background": "#F6F6F6",
+              "@menu-bg": "#CCDCD7",
+              "@table-bg": "@body-background",
+              "@table-header-bg": "#E2E2E2"
+            },
             javascriptEnabled: true,
           },
         },
